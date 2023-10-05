318389494beee9a0fadcf3efb82f7ba6566f706d
Quxen <56299713+QuxenTube@users.noreply.github.com>
2023-10-06 02:40:54 +0300

    triangles homework

---
+++ work2_ex1/work2_ex1.cpp
@@ -0,0 +1,59 @@
+﻿// Create triangles and sandclock
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	for (int i = 0; i <= 10; i++)
+	{
+		for (int j = 0; j <= 10; j++)
+		{
+			if (j == 0 || i == j || i == 10) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << "\n";
+	}
+
+	cout << "\n";
+	cout << "-------------------------------------";
+	cout << "\n";
+	cout << "\n";
+
+	for (int i = 0; i <= 10; i++)
+	{
+		for (int j = 0; j <= 10; j++)
+		{
+			if (j == 10 || i == 10-j || i == 10) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << "\n";
+	}
+
+	cout << "\n";
+	cout << "-------------------------------------";
+	cout << "\n";
+	cout << "\n";
+
+	for (int i = 0; i <= 10; i++)
+	{
+		for (int j = 0; j <= 10; j++)
+		{
+			if (i == 0 || i == 10 || i == j || i == 10 - j) {
+				cout << "*";
+			} else {
+				cout << " ";
+			}
+		}
+		cout << "\n";
+	}
+
+}
