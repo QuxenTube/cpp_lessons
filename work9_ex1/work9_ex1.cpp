fcc0ddbee6f69567280d51caeeedb771d4d0984b
Quxen <56299713+QuxenTube@users.noreply.github.com>
2023-11-13 23:18:16 +0200

    created project

---
+++ work9_ex1/work9_ex1.cpp
@@ -0,0 +1,14 @@
+﻿// code simple text terminal
+//not use string, only char
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    1. интерфейс терминала
+    2. создать функцию сохранения
+    3. Добавить сортировки (13 основных)
+    */
+}
