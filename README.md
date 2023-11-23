e13c4796d836b9713afd5d3bbb2724d0d09af51c
Quxen <56299713+QuxenTube@users.noreply.github.com>
2023-11-23 23:06:02 +0200

    work10_ex1 (1 push)

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 C++ lessons in courses
 
 Check branches for more
+
+### Work 10
+Sorting char array
---
+++ work10_ex1/work10_ex1.cpp
@@ -0,0 +1,33 @@
+﻿// Sorting char array
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    const int len = 100; //length of char array
+	char array[len];
+    cout << "Enter line: \n";
+
+	cin.getline(array, len);
+
+    cout << "\n\n";
+    cout << "Sorted to:";
+    cout << "\n\n\n";
+
+
+    for (int j = 0; j < strlen(array)-1; j++) {
+        for (int i = 0; i < strlen(array)-1; i++) {
+
+            if (int(array[i]) > int(array[i+1])) {
+                swap(array[i], array[i + 1]);
+            }
+        }
+    }
+
+
+    for (int i = 0; i <= strlen(array); i++) {
+        cout << array[i];
+    }
+
+}
