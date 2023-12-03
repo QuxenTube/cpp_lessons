4b751fec797780efe04052a40fb3712a43f4cc73
Quxen <56299713+QuxenTube@users.noreply.github.com>
2023-12-03 02:47:03 +0200

    work11 save before new branch (work not done)

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 C++ lessons in courses
 
 Check branches for more
+
+
+### Work 11
+#### File system
+Implement deleting element(s) from the file, replacing element(s), searching for elements, deleting elements, copying elements, sorting (by all types) + write the logic for auto-sorting elements (with the definition of the optimal algorithm in each case)
---
+++ work11_ex1/work11_ex1.cpp
@@ -0,0 +1,47 @@
+﻿// implement deleting element(s) from the file, replacing element(s), searching for elements, deleting elements, copying elements, sorting (by all types) + write the logic for auto-sorting elements (with the definition of the optimal algorithm in each case)
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void addArray() {
+
+}
+
+int main()
+{
+    char* char_file;
+    ifstream ffile("text.txt", ios::in);
+
+    int n = 100;
+    char* buffer = new char[n + 1];
+    buffer[n] = 0;
+
+    while (!ffile.eof()) {
+        ffile.getline(buffer, n, '\n');
+    }
+
+
+    int symbols = 100;
+
+    for (int i = 0; !ffile.eof(); i++) {
+        ffile.getline(char_file[i], symbols, '\n');
+    }
+
+    char tmp;
+
+
+    ffile >> tmp;
+    cout << tmp << endl;
+
+
+    ffile.getline(buffer, n, '\n');
+    cout << buffer << endl;
+
+    ffile.getline(buffer, n, '\n');
+    cout << buffer << endl;
+
+
+}
